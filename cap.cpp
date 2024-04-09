a58db6738a0293d2c7eaeb3c8aedf78cf8b68efd
Chaitreee <164724143+Chaitreee@users.noreply.github.com>
2024-04-09 19:14:38 +0530

    Update cap.cpp

--- cap.cpp
+++ cap.cpp
@@ -709,3 +709,35 @@ void inputReci(dish &reci)
     }
 }
 
+void searchByCat(char c, int varity)
+{
+    int j = varity, k = 0;
+    for (int i = 0; i < 26; i++)
+    {
+        dish *temp = storage[i].next;
+        while (temp != NULL && j != varity + 5)
+        {
+            if (temp->category == c)
+            {
+                k++;
+                if (k > varity)
+                {
+                    j++;
+                    cout << temp->name << endl;
+                }
+            }
+            temp = temp->next;
+        }
+    }
+    if (j == varity)
+    {
+        if (k == 0)
+        {
+            cout << "Recipe of this category is not available :( " << endl;
+        }
+        else
+        {
+            cout << "No more varites of this categoty is available :(" << endl;
+        }
+    }
+}
